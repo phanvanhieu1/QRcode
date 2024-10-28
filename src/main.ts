55e4035206d96691d102305ab6f19a1b91d1723c
phanvanhieu1 <phanvanhieu12a2@gmail.com>
2024-10-28 23:49:01 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,11 @@ async function bootstrap() {
   }));
   app.setGlobalPrefix('api/v1', {exclude:['']});
   await app.listen(3000);
+  app.use(cors({
+    origin: ['http://localhost:3000', 'https://yourdomain.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  }));
  
 }
 bootstrap();
