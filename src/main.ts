668a5b942b7d5c360ffad92c365beea19332be6d
phanvanhieu1 <phanvanhieu12a2@gmail.com>
2024-10-29 11:53:50 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
   app.setGlobalPrefix('api/v1', {exclude:['']});
-  app.use(cors({
-    origin: ['http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  }));
+  // app.use(cors({
+  //   origin: ['http://localhost:3000'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // }));
   await app.listen(3000);
 }
 bootstrap();
