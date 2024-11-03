96d31de3cff8e8497260c00b06104617254b253d
phanvanhieu1 <phanvanhieu12a2@gmail.com>
2024-11-03 14:03:15 +0700

    aa

--- src/app.module.ts
+++ src/app.module.ts
@@ -44,6 +44,7 @@ export class AppModule implements OnModuleInit {
   onModuleInit() {
     mongoose.connect(process.env.DB_URL).then(() => {
       console.log("connect ok");
+      console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
     })
     .catch(()=>{
       console.log("error")
