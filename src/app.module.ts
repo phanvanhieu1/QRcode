711cdadf88a7ea6c0be82e8d12a612d99cd928c6
phanvanhieu1 <phanvanhieu12a2@gmail.com>
2024-11-08 22:29:46 +0700

    aa

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import mongoose, { connection } from 'mongoose';
-import { CategoryModule } from './modules/category/category.module';
+import mongoose from 'mongoose';
 import { ComboModule } from './modules/combo/combo.module';
 import { OrderModule } from './modules/order/order.module';
 import { ProductModule } from './modules/product/product.module';
@@ -13,8 +12,6 @@ import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
-import { MulterModule } from '@nestjs/platform-express';
-import { S3Client } from '@aws-sdk/client-s3';
 import 'dotenv/config';
 
 @Module({
@@ -25,7 +22,6 @@ import 'dotenv/config';
     }),
     
     MongooseModule.forRoot(process.env.DB_URL),
-    CategoryModule,
     ComboModule,
     OrderModule,
     ProductModule ,
