210b866700ef33b7563011956aaa35daea8ce08f
phanvanhieu1 <phanvanhieu12a2@gmail.com>
2024-11-08 22:59:31 +0700

    c

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// import * as cors from 'cors';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
   app.setGlobalPrefix('api/v1', {exclude:['']});
-  await app.listen(process.env.PORT, "0.0.0.0");
+  await app.listen(port, "0.0.0.0");
 }  
 bootstrap(); 
